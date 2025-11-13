acb5cdd051a902ef3c0fab507da4318963dc22e1
hoduchuy2011 <guchiguguchi2011@gmail.com>
2025-11-13 22:23:53 +0700

    13/11/2025

---
+++ done/31/khmm.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define st string
+#define fi first
+#define se second
+#define mod 1000000007
+#define all(x) x.begin(), x.end()
+
+using namespace std;
+
+void open(string s)
+{
+	if(fopen((s + ".INP").c_str(), "r"))
+	{
+		freopen((s + ".INP").c_str(), "r", stdin);
+		freopen((s + ".OUT").c_str(), "w", stdout);
+	}
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+}
+signed main()
+{
+	open("khmm");
+	int n, x, y;
+	cin >> n >> x >> y
+	vector<int> b(n);
+	vector<int> s(n);
+	for (int i = 0; i < n; ++i)
+	{
+		int v;
+		cin >> v;
+		b[i] = abs(v);
+		s[i] = (v > 0) ? 1 : -1;
+	}
+
+	int l = 0;
+	int p = 0, g = 0;
+	int k = -1, m = -1;
+	int w = INT_MAX;
+
+	for (int r = 0; r < n; ++r)
+	{
+		if (s[r] == 1)
+		{
+			++p;
+		}
+		else
+		{
+			++g;
+		}
+		while (l <= r && p >= x && g >= y)
+		{
+			int t = b[r] - b[l];
+			if (t < w || (t == w && (k == -1 || b[l] < b[k])))
+			{
+				w = t;
+				k = l;
+				m = r;
+			}
+			if (s[l] == 1)
+			{
+				--p;
+			}
+			else
+			{
+				--g;
+			}
+			++l;
+		}
+	}
+
+	if (k == -1)
+	{
+		cout << -1 << '\n';
+	}
+	else
+	{
+		cout << b[k] << ' ' << b[m] << '\n';
+	}
+	return 0;
+}
--- tmo.cpp
+++ tmo.cpp
@@ -8,42 +8,78 @@
 
 using namespace std;
 
-void open(st s)
+void open(string s)
 {
-    if(fopen((s + ".INP").c_str(), "r"))
-    {
-        freopen((s + ".INP").c_str(), "r", stdin);
-        freopen((s + ".OUT").c_str(), "w", stdout);
-    }
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+	if(fopen((s + ".INP").c_str(), "r"))
+	{
+		freopen((s + ".INP").c_str(), "r", stdin);
+		freopen((s + ".OUT").c_str(), "w", stdout);
+	}
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 }
-
 signed main()
 {
-    open(""); 
-    ll n, k; 
-    cin>>n>>k;
-    vector<ll> a(n), f(n); 
-    for(ll i = 0; i < n; i++)
+	open("");
+
+    int n, x, y;
+    if (!(cin >> n >> x >> y))
     {
-        cin>>a[i];
+        return 0;
     }
-    ll s = 0;
-    for(ll i = 0; i < n; i++)
+    vector<int> b(n);
+    vector<int> s(n);
+    for (int i = 0; i < n; ++i)
     {
-        f[i] = 1;
-        for(ll j = 0; j < i; j++)
+        int v;
+        cin >> v;
+        b[i] = abs(v);
+        s[i] = (v > 0) ? 1 : -1;
+    }
+
+    int l = 0;
+    int p = 0, g = 0;
+    int k = -1, m = -1;
+    int w = INT_MAX;
+
+    for (int r = 0; r < n; ++r)
+    {
+        if (s[r] == 1)
         {
-            if(a[i] - a[j] >= k)
-            {
-                f[i] = max(f[i], f[j] + 1);
-            }
+            ++p;
         }
-        s = max(s, f[i]); 
+        else
+        {
+            ++g;
+        }
+        while (l <= r && p >= x && g >= y)
+        {
+            int t = b[r] - b[l];
+            if (t < w || (t == w && (k == -1 || b[l] < b[k])))
+            {
+                w = t;
+                k = l;
+                m = r;
+            }
+            if (s[l] == 1)
+            {
+                --p;
+            }
+            else
+            {
+                --g;
+            }
+            ++l;
+        }
     }
-    cout<<s; 
-    
-    cerr<<"Time elapsed: "<<1.0 * clock() / CLOCKS_PER_SEC<<".s\n";
+
+    if (k == -1)
+    {
+        cout << -1 << '\n';
+    }
+    else
+    {
+        cout << b[k] << ' ' << b[m] << '\n';
+    }
     return 0;
 }
