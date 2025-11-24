a7f7d457846c7b58f7a88501909406ee2206e935
Lunaris <guchiguguchi2011@gmail.com>
2025-11-24 16:12:38 +0700

    24/11/2025

--- code.cpp
+++ code.cpp
@@ -18,9 +18,8 @@ void open(string s)
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 }
-signed main()
+void sub1()
 {
-	open("");
 	ll n, k, p, q, m, c;
 	cin>>n>>k>>p>>q>>m;
 	ll t = t = n * (n + 1) * k / 2;
@@ -42,6 +41,43 @@ signed main()
 		res = min(res, tmp);
 	}
 	cout<<res;
+}
+void sub2()
+{
+	ll n, d;
+	cin>>n>>d;
+	ll p, q, m;
+	cin >> p >> q >> m;
+	vector<ll> a(n + 1);
+	for (ll i = 1; i <= n; i++)
+	{
+		a[i] = (p * i) % m + q;
+	}
+	multiset<ll> s;
+	for (ll i = 1; i <= n; i++)
+	{
+		s.insert(i * d + a[i]);
+	}
+	ll r = *s.rbegin();
+	for (ll i = 2; i <= n; i++)
+	{
+		ll x = (i - 1) * d + a[i - 1];
+		auto it = s.find(x);
+		s.erase(it);
+
+		ll k = i + n - 1;
+		ll v = k * d + a[(k - 1) % n + 1];
+		s.insert(v);
+
+		ll c = *s.rbegin() - (i - 1) * d;
+		if (c < r) r = c;
+	}
+	cout << r;
+}
+signed main()
+{
+	open("");
+	sub2();
 	
 	cerr<<"Time elapsed: "<<1.0 * clock() / CLOCKS_PER_SEC<<".s\n";
 	return 0;
